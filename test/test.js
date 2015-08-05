f9388fe11444d1aa13aba27898aac0aaf6d6e0f2
calidion <calidion@gmail.com>
2015-08-05 17:41:03 +0800

    updated to get rid of travis failure

--- test/test.js
+++ test/test.js
@@ -4,8 +4,7 @@ var nodeOrder = require('../');
 
 describe('node-order node module', function () {
   it('It should generate order id', function () {
-    var id = require('../lib/id');
-    var no = id.generate(6);
+    var no = nodeOrder.id.generate(6);
     var a = no.split('-');
     assert(true, /[0-9]{14}/.test(a[0]));
     assert(true, /[A-Z]{6}/.test(a[1]));
