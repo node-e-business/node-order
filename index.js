84df741e43c8af7a736bb9235ce325d6c8a40d3d
calidion <calidion@gmail.com>
2015-08-05 17:32:41 +0800

    enabled id generation

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 'use strict';
-module.exports = function (str) {
-  console.log(str || 'Rainbow');
+
+
+module.exports =  {
+  id: require('./lib/id')
 };
--- test/test.js
+++ test/test.js
@@ -3,8 +3,11 @@ var assert = require('assert');
 var nodeOrder = require('../');
 
 describe('node-order node module', function () {
-  it('This it the first test should be passed.', function () {
-    nodeOrder();
-    assert(true, 'Initial test added');
+  it('It should generate order id', function () {
+    var id = require('../lib/id');
+    var no = id.generate(6);
+    var a = no.split('-');
+    assert(true, /[0-9]{14}/.test(a[0]));
+    assert(true, /[A-Z]{6}/.test(a[1]));
   });
 });
